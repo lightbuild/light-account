db875169b97ae3ed97e8a3bb3446db4db26da2cb
lightbuild <renran6303@gmail.com>
2022-04-13 16:00:14 +0800

    页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,88 @@
 <template>
-  <div>
-    money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -9,9 +91,19 @@
 
 export default {
   name: 'Money',
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
