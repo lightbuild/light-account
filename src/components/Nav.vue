2f6ae6fe9915559ea7851ec683ca34bcd4185546
lightbuild <renran6303@gmail.com>
2022-04-13 17:43:33 +0800

    使用svg-loader

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/label.svg'
+import y from '@/assets/icons/money.svg'
+import z from '@/assets/icons/statistics.svg'
+
+console.log(y);
+console.log(z);
+console.log(x);
 export default {
   name: 'Nav'
 };
