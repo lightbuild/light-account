5677752f855ade65342101c0d644a94126a74f4f
lightbuild <renran6303@gmail.com>
2022-04-13 14:03:15 +0800

    全局组件 <Nav>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     labels.vue
+    <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Labels'
+  name: 'Labels',
 }
 </script>
 
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     statistics.vue
+    <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Statistics'
+  name: 'Statistics',
 }
 </script>
 
