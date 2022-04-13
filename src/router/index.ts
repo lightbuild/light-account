b40d0b4602ffce9cbdd26f289f92de468698c3d1
lightbuild <renran6303@gmail.com>
2022-04-13 14:15:18 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFind from '@/views/NotFind.vue';
 
 
 
@@ -24,6 +25,10 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component:Statistics
+  },
+  {
+    path:'*',
+    component:NotFind
   }
 ];
 
