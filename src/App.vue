2197f66524288705ba184f7669694c62befc3bd9
lightbuild <renran6303@gmail.com>
2022-04-15 11:26:05 +0800

    完成money页面的HTML和CSS

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
   body{
     line-height: 1.5;
     font-family: $font-hei;
+    font-size: 16px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #333;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link class="item" to="/money" active-class="selected">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link class="item" to="/labels" active-class="selected">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link class="item" to="/statistics" active-class="selected">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -17,31 +17,31 @@
 
 <script lang="ts">
   export default {
-    name: 'Nav',
+    name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
-  nav{
+  nav {
+    @extend %outerShadow;
     display: flex;
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
     flex-direction: row;
     font-size: 12px;
-        > .item {
-          padding: 2px 0;
-          width: 33.33333%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-direction: column;
-          .icon {
-            width: 32px;
-            height: 32px;
-          }
+    > .item {
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
     }
-       > .item.selected{
-         color: $color-highlight;
-       }
+    > .item.selected{
+      color: $color-highlight;
+    }
   }
 </style>
