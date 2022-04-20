f80b8339fe7ecb56b74b7fe2610eff5c4ea57e3c
lightbuild <renran6303@gmail.com>
2022-04-20 11:01:28 +0800

    添加标签可跳转至编辑页面

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    编辑标签
+    <div>编辑页面</div>
   </Layout>
 </template>
 
@@ -14,7 +14,7 @@
       const id = this.$route.params.id;
       tagListModel.fetch();
       const tags = tagListModel.data;
-      const tag = tags.filter(t => t.id === id)[0];
+      const tag = tags.find(t => t.id === id);
       if (tag){
         console.log(tag);
       }else {
